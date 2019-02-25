cea128dc2272ecaa733ac4913f807db8ad15cd9f
yy <171178403@qq.com>
2019-02-25 15:23:15 +0800

    '2.25下午'

--- projectend/routes/classfiyApi/index.js
+++ projectend/routes/classfiyApi/index.js
@@ -1,6 +1,7 @@
 var mongodb = require('mongodb-curd');
 var dbBase = 'ningmeng';
 var dbCol = 'iconname';
+var classifyCol = 'classify';
 //获取所有icon
 var iconname = function (req, res, next) {
     mongodb.find(dbBase, dbCol, {}, function (result) {
@@ -22,12 +23,28 @@ var classifyuser = function(req, res, next) {
       type = params.type,
       uid = params.uid;
 if (!iname || !cname || !type || !uid) {
-  res.send({code: 3, msg: '请完善用户信息'})
+  res.send({code: 2, msg: '缺少参数'})
 } else {
   getusermsg();
 }
 function getusermsg () {
-  
+    mongodb.find(dbBase, classifyCol, {cname: cname, type: type, uid: {$in: ["*", uid]}}, function (result) {
+      if (result.length > 0) {
+          res.send({code: 3, msg: '该分类已经存在'})
+      } else {
+          addClass();
+      }
+    })
+}
+
+function addClass () {
+  mongodb.insert({dbBase, classifyCol, params, function (result) {
+    if (result) {
+      res.send({code: 0, msg: '插入分类成功'})
+    } else {
+      res.send({code: 1, msg: '插入分类失败'})
+    }
+  }})
 }
 
 }
